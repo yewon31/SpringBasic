6936a652b9a2d78e903a996c26a90888bc63919d
Yewon <qjqmti0113@gmail.com>
2024-08-06 12:03:27 +0900

    0806

--- src/main/java/com/simple/basic/service/score/ScoreServiceImpl.java
+++ src/main/java/com/simple/basic/service/score/ScoreServiceImpl.java
@@ -2,35 +2,46 @@ package com.simple.basic.service.score;
 
 import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.simple.basic.command.ScoreVO;
 import com.simple.basic.service.score.impl.ScoreDAO;
+import com.simple.basic.util.aop.LogAdvice;
 
-@Service("x") // 반드시 component-scan에 의하여 읽혀야 함
+@Service("x") //반드시 component-scan에 의하여 읽혀야 함
 public class ScoreServiceImpl implements ScoreService {
-
+	
+	//public static final Logger log = LoggerFactory.getLogger(ScoreServiceImpl.class);
+	
 //	@Autowired
 //	@Qualifier("y")
 //	ScoreDAO scoreDAO;
-
+		
 	@Autowired
 	private ScoreMapper scoreMapper;
-
+		
+	
 	@Override
 	public void regist(ScoreVO vo) {
+		System.out.println("regist메서드 동작");
 		scoreMapper.regist(vo);
 	}
 
+
 	@Override
 	public ArrayList<ScoreVO> getScore() {
+		System.out.println("getScore메서드 동작");
 		return scoreMapper.getScore();
 	}
 
+
 	@Override
 	public void delete(int sno) {
+		System.out.println("delete메서드 동작");
 		scoreMapper.delete(sno);
 	}
 
--- src/main/java/com/simple/basic/util/aop/LogAdvice.java
+++ src/main/java/com/simple/basic/util/aop/LogAdvice.java
@@ -1,16 +1,26 @@
 package com.simple.basic.util.aop;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Arrays;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect //AOP클래스
 @Component //이 클래스는 component-scan에 읽혀서 bean으로 생성됨
 public class LogAdvice {
+	
+	//해당 클래스에서 로그를 사용함 (base패키지 밑에 존재해야 사용합니다.)
+	public static final Logger log = LoggerFactory.getLogger(LogAdvice.class);
+
 	
 	//advice - 공통기능
 	//point cut - 내가 적용할 메서드에 advice 부착하는 행위
@@ -40,7 +50,6 @@ public class LogAdvice {
 	//@Around - after, before, afterThrowing 세가지를 결합해서, 처리할 수 있게 해주는 어노테이션
 	//규칙 - 반환은 Object으로 만들고, 매개변수로 실행시점을 결정지어줄 수 있는 ProceedingJoinPoint를 선언합니다.
 	
-	
 	@Around("execution(* com.simple.basic.service.score.*ScoreServiceImpl.*(..))")
 	public Object arroundLog(ProceedingJoinPoint jp) {
 		
@@ -51,25 +60,44 @@ public class LogAdvice {
 		
 		*/
 		
+		long start = System.currentTimeMillis();
+				
+		log.info( "실행클래스:"  + jp.getTarget() );
+		log.info( "실행메서드:"  + jp.getSignature().toString());
+		log.info( "매개값:" + Arrays.toString(jp.getArgs() ));
+				
 		Object result = null;
 		
 		try {
-			
+						
+			//before
 			result = jp.proceed(); //타겟메서드의 실행
+			//after
+							
+			long end = System.currentTimeMillis();
+			log.info( "실행시간:" + (end - start) * 0.001 );
+			
+			
+			String path = "C:\\Users\\user\\Desktop\\course\\spring\\spring_log\\오늘날짜_log.txt";
+			BufferedWriter bos = new BufferedWriter( new FileWriter(path, true));
+			bos.write( "실행클래스:"  + jp.getTarget() + "\n"); //파일을 씀
+			bos.write( "실행메서드:"  + jp.getSignature().toString()  + "\n");
+			bos.write( "매개값:" + Arrays.toString(jp.getArgs())  + "\n" );
+			bos.write( "실행시간:" + (end - start) * 0.001   + "\n");
+			bos.flush();
+			
+			
+			bos.close();
+			
 			
 		} catch (Throwable e) {
 			e.printStackTrace(); //해당 메서드에서 에러가 발생하면, 이친구가 로그를 같이 찍음
-		}
-		
+		} 
 		
 		return result; //메서드의 정상 흐름으로 돌아갑니다.
 	}
 	
-	
-	
-	
-	
-	
+
 	
 	
 	
